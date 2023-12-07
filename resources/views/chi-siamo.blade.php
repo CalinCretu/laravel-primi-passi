a8688668f6b3173536b2625b906c5cd10e316224
CalinCretu <calincretu1412@gmail.com>
2023-12-07 17:36:29 +0100

    FIX - indent

--- resources/views/chi-siamo.blade.php
+++ resources/views/chi-siamo.blade.php
@@ -1,70 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Chi siamo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chi siamo</title>
 </head>
+
 <body>
-  <div class="header">
-    <a href="{{ route('home') }}" target="_blank" rel="noopener noreferrer">Home</a>
-    <a href="{{ route('lorem-ipsum') }}" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
-  </div>
-  <div class="container">
-    <h2>{{ $title }}</h2>
-  <ul class="list">
-    @foreach ($users as $user)
-        <li><a class="names" href="http://" target="_blank" rel="noopener noreferrer">{{ $user }}</a></li>
-    @endforeach
-  </ul>
-  </div>
-  
+    <div class="header">
+        <a href="{{ route('home') }}" target="_blank" rel="noopener noreferrer">Home</a>
+        <a href="{{ route('lorem-ipsum') }}" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+    </div>
+    <div class="container">
+        <h2>{{ $title }}</h2>
+        <ul class="list">
+            @foreach ($users as $user)
+                <li><a class="names" href="http://" target="_blank" rel="noopener noreferrer">{{ $user }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
 </body>
+
 </html>
 
 <style>
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-  .header{
-  margin: 10px auto;
-    display: flex;
-    justify-content: center;
-    max-width: 800px;
-}
-.container{
-    margin: 10px auto;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    max-width: 400px;
-  }
-  .list {
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
-  }
-  a{
-   margin: 0 10px;
-   text-decoration: none;
-   color: black;
-   border: 1px solid black;
-   padding: 5px 10px;
-  }
+    .header {
+        margin: 10px auto;
+        display: flex;
+        justify-content: center;
+        max-width: 800px;
+    }
 
-  a:hover{
-    background-color: black;
-    color: white;
-  }
+    .container {
+        margin: 10px auto;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        max-width: 400px;
+    }
 
-  ul,ol,menu{
-    list-style: none;
-  }
-  li {
-    margin: 10px 0;
-  }
-  .names {
-    border: none;
-  }
+    .list {}
+
+    a {
+        margin: 0 10px;
+        text-decoration: none;
+        color: black;
+        border: 1px solid black;
+        padding: 5px 10px;
+    }
+
+    a:hover {
+        background-color: black;
+        color: white;
+    }
+
+    ul,
+    ol,
+    menu {
+        list-style: none;
+    }
+
+    li {
+        margin: 10px 0;
+    }
+
+    .names {
+        border: none;
+    }
 </style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,56 +2,59 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
 </head>
 
 <body>
-  <div class="header">
-    <div>
-      <a href="{{ route('chi-siamo') }}" target="_blank" rel="noopener noreferrer">Chi siamo</a>
+    <div class="header">
+        <div>
+            <a href="{{ route('chi-siamo') }}" target="_blank" rel="noopener noreferrer">Chi siamo</a>
+        </div>
+        <div>
+            <a href="{{ route('lorem-ipsum') }}" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+        </div>
+
     </div>
-    <div>
-      <a href="{{ route('lorem-ipsum') }}" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+    <div class="container">
+        <h1>Hello World!!!!!</h1>
     </div>
-     
-  </div>
-  <div class="container">
-    <h1>Hello World!!!!!</h1>
-  </div>
 </body>
 
 </html>
 
 <style>
-  *{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-.header{
-  margin: 20px auto;
-    display: flex;
-    justify-content: center;
-    max-width: 800px;
-}
-.container{
-    margin: 10px auto;
-    display: flex;
-    justify-content: center;
-    max-width: 400px;
-  }
-  a{
-   margin: 0 10px;
-   text-decoration: none;
-   color: black;
-   border: 1px solid black;
-   padding: 5px 10px;
-  }
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
-  a:hover{
-    background-color: black;
-    color: white;
-  }
+    .header {
+        margin: 20px auto;
+        display: flex;
+        justify-content: center;
+        max-width: 800px;
+    }
+
+    .container {
+        margin: 10px auto;
+        display: flex;
+        justify-content: center;
+        max-width: 400px;
+    }
+
+    a {
+        margin: 0 10px;
+        text-decoration: none;
+        color: black;
+        border: 1px solid black;
+        padding: 5px 10px;
+    }
+
+    a:hover {
+        background-color: black;
+        color: white;
+    }
 </style>
